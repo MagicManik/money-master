d1ebf9259e8c1f4341935787acd7d0143f8c5a87
MagicManik <manikislam1952@gmail.com>
2022-02-17 20:28:32 +0600

    Total Expense and Balance Part Done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,62 @@
-document.getElementById('calculate-btn').addEventListener('click', function () {
-    console.log('click income and expenses')
-});
-
-document.getElementById('saving-btn').addEventListener('click', function () {
-    console.log('click saving')
+document.getElementById('calculate-button').addEventListener('click', function () {
+    updateBalance();
 })
+
+
+
+
+function getInputValue(inputId) {
+
+    const inputField = document.getElementById(inputId);
+    const inputValue = inputField.value;
+    const newInputAmount = parseInt(inputValue);
+
+    if (newInputAmount < 0) {
+        alert('You can not input the negative number here');
+        return;
+    }
+    else if (isNaN(newInputAmount)) {
+        alert('please input the number');
+        return;
+    }
+    return newInputAmount;
+}
+
+
+function totalExpenses() {
+
+    const foodCost = getInputValue('food-cost');
+    const rentCost = getInputValue('rent-cost');
+    const clothCost = getInputValue('cloth-cost');
+    const totalExpense = foodCost + rentCost + clothCost;
+    const incomeAmount = getInputValue('income-input');
+
+    if (totalExpense > incomeAmount) {
+        alert('You have not sufficient amount to expense more');
+        return;
+    }
+    else if (isNaN(totalExpense)) {
+        return;
+    }
+    const expenseField = document.getElementById('expense-field');
+    expenseField.innerText = totalExpense;
+
+    return totalExpense;
+}
+
+
+function updateBalance() {
+    const incomeAmount = getInputValue('income-input');
+    const totalExpense = totalExpenses();
+
+    const totalBalance = incomeAmount - totalExpense;
+
+
+    if (isNaN(totalBalance)) {
+        return;
+    }
+    const balanceFieldAmount = document.getElementById('total-balance');
+    balanceFieldAmount.innerText = totalBalance;
+
+    return totalBalance;
+}
