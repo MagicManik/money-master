fcca8d138ceff97ccd755d805313879f19beaac1
MagicManik <manikislam1952@gmail.com>
2022-02-17 21:24:32 +0600

    Almost Done

--- js/main.js
+++ js/main.js
@@ -1,62 +1,122 @@
-document.getElementById('calculate-button').addEventListener('click', function () {
+// calculate event handler or calculate button
+document.getElementById('calculate-btn').addEventListener('click', function () {
     updateBalance();
-})
+});
 
 
+// saving amount event handler or saving button
+document.getElementById('saving-btn').addEventListener('click', function () {
+    const savingsAmount = savings();
+    const balanceTotal = updateBalance();
 
-
-function getInputValue(inputId) {
-
-    const inputField = document.getElementById(inputId);
-    const inputValue = inputField.value;
-    const newInputAmount = parseInt(inputValue);
-
-    if (newInputAmount < 0) {
-        alert('You can not input the negative number here');
+    if (savingsAmount > balanceTotal) {
+        alert('You have not sufficient balance to save');
         return;
     }
-    else if (isNaN(newInputAmount)) {
-        alert('please input the number');
+
+    // remaining blance
+    const remainingBalance = balanceTotal - savingsAmount;
+
+    // handle NaN error
+    if (isNaN(remainingBalance)) {
         return;
     }
-    return newInputAmount;
-}
+
+    // set update remaining blance on remaining blance
+    const setRemainingBlance = document.getElementById('remaining-balance');
+    setRemainingBlance.innerText = remainingBalance;
+});
 
 
-function totalExpenses() {
 
+//update balance after expenses
+function updateBalance() {
+    const incomeAmount = getInputValue('income-input');
+
+    // expense sectors
     const foodCost = getInputValue('food-cost');
     const rentCost = getInputValue('rent-cost');
     const clothCost = getInputValue('cloth-cost');
-    const totalExpense = foodCost + rentCost + clothCost;
-    const incomeAmount = getInputValue('income-input');
 
+    // total expense
+    const totalExpense = foodCost + rentCost + clothCost;
+
+    // handle over expense input than income amount
     if (totalExpense > incomeAmount) {
         alert('You have not sufficient amount to expense more');
         return;
     }
+    // handle NaN error
     else if (isNaN(totalExpense)) {
         return;
     }
-    const expenseField = document.getElementById('expense-field');
+
+    // update total expense
+    const expenseField = document.getElementById('total-expenses');
     expenseField.innerText = totalExpense;
 
-    return totalExpense;
-}
-
-
-function updateBalance() {
-    const incomeAmount = getInputValue('income-input');
-    const totalExpense = totalExpenses();
-
+    // Balance
     const totalBalance = incomeAmount - totalExpense;
 
-
+    // handle NaN error
     if (isNaN(totalBalance)) {
         return;
     }
+
+    // update balance
     const balanceFieldAmount = document.getElementById('total-balance');
     balanceFieldAmount.innerText = totalBalance;
 
     return totalBalance;
-}
+};
+
+
+
+
+// to get input value of every input
+function getInputValue(inputId) {
+
+    // access input field
+    const inputField = document.getElementById(inputId);
+    const inputValue = inputField.value;
+    const newInputAmount = parseInt(inputValue);
+
+    // handle 'negative' input
+    if (newInputAmount < 0) {
+        alert('You can not input the negative number here');
+        return;
+    }
+
+    // handle 'string' input
+    else if (isNaN(newInputAmount)) {
+        alert('please input the number');
+        return;
+    }
+    else {
+        return newInputAmount;
+    }
+};
+
+
+
+
+
+
+// savings part
+function savings() {
+    const incomeAmount = getInputValue('income-input');
+    const savingsPercent = getInputValue('save-input');
+
+    // get savings amount
+    const savings = (incomeAmount * savingsPercent) / 100;
+
+    // handle NaN error
+    if (isNaN(savings)) {
+        return;
+    }
+    // update savings amount
+    const saveField = document.getElementById('saving-amount');
+    saveField.innerText = savings;
+
+    return savings;
+};
